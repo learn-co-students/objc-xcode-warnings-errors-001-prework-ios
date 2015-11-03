af4b8ee2d789cbba360dfd88b681b3b36ccaa163
dominicprescod <dominicprescod@gmail.com>
2015-11-02 21:27:47 -0500

    Warnings

--- objc-xcode-warnings-errors/FISAppDelegate.m
+++ objc-xcode-warnings-errors/FISAppDelegate.m
@@ -5,17 +5,23 @@
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+   
+    NSString *message = @"Even though they don't belong here, the compiler won't actually complain about string literals or primitives defined outside of a method body (which is held by  curly braces {...} ), but...";
+    
+    NSInteger j = 0;
+    BOOL itIsKnownKhaleesi = YES;
+ 
     // Override point for customization after application launch.
     
     NSString *unused = @"This variable generates a warning because it is unused.";
     
-//    NSLog(@"%@", unused);
+    NSLog(@"%@", unused);
     
-//    NSInteger *i = 0;
-//    NSLog(@"i: %li", i);
+    NSInteger i = 12;
+    NSLog(@"i: %li", i);
     
-//    NSInteger x = i + 1;
-//    NSLog(@"x: %li", x);
+    NSInteger x = i + 1;
+    NSLog(@"x: %li", x);
     
     NSLog(@"Anything after the return statement will not get executed.");
     
@@ -24,12 +30,8 @@
     NSLog(@"Take note that this line doesn't print to the console.");
 }
 
-//NSString *message = @"Even though they don't belong here, the compiler won't actually complain about string literals or primitives defined outside of a method body (which is held by  curly braces {...} ), but...";
 
-//NSInteger j = 0;
-//BOOL itIsKnownKhaleesi = YES;
-
-//NSLog(@"...any statements containing function calls, operations, or method calls will produce errors.");
+//  NSLog(@"...any statements containing function calls, operations, or method calls will produce errors.");
 
 
 
