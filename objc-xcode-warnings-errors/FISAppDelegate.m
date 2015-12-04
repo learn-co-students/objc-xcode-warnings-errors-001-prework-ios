6981de3493025a34d32673799c4511894986aebb
Shak-2-system <waterproof15@gmail.com>
2015-12-04 07:56:03 -0500

    finished curly cue stuff

--- objc-xcode-warnings-errors/FISAppDelegate.m
+++ objc-xcode-warnings-errors/FISAppDelegate.m
@@ -22,27 +22,27 @@
     return YES; // this line ends the method
     
     NSLog(@"Take note that this line doesn't print to the console.");
+
+
+NSString *message = @"Even though they don't belong here, the compiler won't actually complain about string literals or primitives defined outside of a method body (which is held by  curly braces {...} ), but...";
+
+NSInteger j = 0;
+BOOL itIsKnownKhaleesi = YES;
+
+NSLog(@"...any statements containing function calls, operations, or method calls will produce errors.");
+
+
+
+
+NSLog(@"%@", message);
+
+
+
+j++;
+
+itIsKnownKhaleesi = NO;
+
+
+NSString *notLocal = @"Which means the variables above, while permitted, can't be used in the way that you intend.";
 }
-
-//NSString *message = @"Even though they don't belong here, the compiler won't actually complain about string literals or primitives defined outside of a method body (which is held by  curly braces {...} ), but...";
-
-//NSInteger j = 0;
-//BOOL itIsKnownKhaleesi = YES;
-
-//NSLog(@"...any statements containing function calls, operations, or method calls will produce errors.");
-
-
-
-
-//NSLog(@"%@", message);
-
-
-
-//j++;
-
-//itIsKnownKhaleesi = NO;
-
-
-//NSString *notLocal = [NSString stringWithString:@"Which means the variables above, while permitted, can't be used in the way that you intend."];
-
 @end
